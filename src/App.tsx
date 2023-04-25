6aebf1a76a93e06e6c4a99e5f9c422069252f2d8
Anand Manikkam <anandmanikkam@UserDatas-MacBook-Pro-3.local>
2023-04-25 13:53:41 +0800

    base styles configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-
-
 function App() {
-  return <h1>Sample</h1>;
+  return (
+    <div>
+      <h1>Sample</h1>
+      <button className="btn btn-primary">Sample</button>
+    </div>
+  );
 }
 
 export default App;
