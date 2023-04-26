e6bc7d4ebb4c164ae901dd743c5feef4b4d52b45
Razi-abedi <razieh.a@userdata.tech>
2023-04-26 17:55:10 +0800

    added a sample navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import NavBar from './components/NavBar';
+import React from 'react';
 
-
-function App() {
-  return <div>
- <h1>Sample</h1>
- <h2>test1</h2>
- <h4>test2</h4>
- <h4>test3</h4>
- <p>hi</p>
-  </div>
-  
+const App: React.FC = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Sample</h1>
+      <h2>test1</h2>
+      <h4>test2</h4>
+      <h4>test3</h4>
+      <p>hi</p>
+    </div>
+  );
 }
 
 export default App;
+
+// import NavBar from './components/NavBar';
+
+// function App() {
+//   return <div>
+//     <NavBar/>
+//  <h1>Sample</h1>
+//  <h2>test1</h2>
+//  <h4>test2</h4>
+//  <h4>test3</h4>
+//  <p>hi</p>
+//   </div>
+  
+// }
+
+// export default App;
